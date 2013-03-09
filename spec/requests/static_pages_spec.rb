e0e87ff9729bcbac24b0e91fb00c31204393b897
Avi Romascanu <aromasca@gmail.com>
2013-03-09 16:11:49 +0200

    using the ruby helper to build the title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,9 +10,14 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
       page.should have_selector('h1', :text => 'Sample App')
     end
 
+      it "should have the base title" do
+        visit '/static_pages/home'
+        page.should have_selector('title', :text => "#{base_title}")
+      end
+
       it "should have the right title" do
         visit '/static_pages/home'
-        page.should have_selector('title', :text => "#{base_title} | Home")
+        page.should have_selector('title', :text => "| Home")
       end
 
     end
